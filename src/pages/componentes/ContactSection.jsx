e689d3c0621e7aff8cbc59dd127f191482344b7a
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2024-07-16 20:08:33 -0300

    Ajustes

--- src/pages/componentes/ContactSection.jsx
+++ src/pages/componentes/ContactSection.jsx
@@ -5,6 +5,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { FaGithub, FaEnvelope, FaLinkedin } from "react-icons/fa6";
 
+//porque esse id do contact
+
 const Contact = () => {
     return (
         <>
